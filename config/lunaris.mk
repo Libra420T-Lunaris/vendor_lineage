507860cbdf96c508d5b1ca7c6c822139bfc9803b
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    config: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/lunaris.mk
+++ config/lunaris.mk
@@ -12,3 +12,7 @@ ifeq ($(WITH_GMS),false)
 PRODUCT_PACKAGES += \
     UpdaterVanillaOverlay
 endif
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-lunaris-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-lunaris-product.xml
